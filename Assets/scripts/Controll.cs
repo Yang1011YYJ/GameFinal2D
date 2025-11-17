ef441fdfbe33860b82b5697eaae45180d48933d5
Yang1011YYJ <YangYongJie1011@gmail.com>
2025-11-18 03:13:24 +0800

    Initial commit (Unity project with LFS and .gitignore)

---
+++ Assets/scripts/Controll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controll : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer sprite;
+    public Rigidbody rig;
+   
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        rig = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rig.AddForce(new Vector2(x * 10, 0));
+
+    }
+}
