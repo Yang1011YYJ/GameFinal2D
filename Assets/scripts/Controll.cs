0f2864f57d9d4cee65bc21b927a34ab7e4e62a21
YangYYJ <YangYongJie1011@gmail.com>
2025-11-22 00:55:43 +0800

    人物移動優化

--- Assets/scripts/Controll.cs
+++ Assets/scripts/Controll.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Controll : MonoBehaviour
 {
     public Animator animator;
-    public SpriteRenderer sprite;
+
+    [Header("角色外表")]
+    public SpriteRenderer spriteRenderer;
+    [Tooltip("面正面")]
+    public Sprite image_0;
+    [Tooltip("面左邊")]
+    public Sprite image_1;
+    [Tooltip("面背面")]
+    public Sprite image_2;
+
     public Rigidbody rig;
    
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
-        sprite = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         rig = gameObject.GetComponent<Rigidbody>();
     }
 
@@ -19,7 +28,39 @@ public class Controll : MonoBehaviour
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        rig.AddForce(new Vector3(x * 5, 0));
+        // 角色移動
+        rig.AddForce(new Vector2(x * 5f, 0f));
 
+        //限制速度最大值
+        rig.velocity = new Vector2(
+            Mathf.Clamp(rig.velocity.x, -10f, 10f),
+            rig.velocity.y
+        );
+        print(rig.velocity);
+
+        //動畫設定(判斷是否在走路)
+        if (x != 0)
+        {
+            animator.SetBool("walk", true);
+            // 走路時使用側面圖
+            spriteRenderer.sprite = image_1;
+            if (x > 0)
+            {
+                //sprite.flipX = true;
+                //角色面向右邊
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+            // 沒在移動，用正面圖
+            spriteRenderer.flipX = false;      // 正面圖通常不用翻
+            spriteRenderer.sprite = image_0;
+        }
     }
 }
